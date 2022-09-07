dcf6a3563fc83dc39976b7e80d30b86a74e8af82
1b0325h <melano@1b0325h.p001.jp>
2022-09-08 04:22:05 +0900

    Create stirge.py

---
+++ stirge.py
@@ -0,0 +1,42 @@
+import random
+import string
+import os
+
+from colorama import Fore, init
+import fire
+
+
+
+def Stirge(dst=None):
+    print("This program will randomize all file names in a directory."\
+          "\nDo not use in wrong directory.",)
+
+    if not dst:
+        dst = input("\nEnter the full path of the directory to randomized: ")
+
+    # directory check
+    if not os.path.isdir(dst):
+        return Fore.RED + "\nERROR: Directory does not exist."
+
+    print(Fore.GREEN + f"\nRenaming all files in {dst}")
+
+    if input("\nDo you want to run it in this directory? [y/N]: ").lower() in ["y", "ye", "yes"]:
+        for i in os.listdir(dst):
+            src = os.path.join(dst, i)
+            if os.path.isfile(src):
+                extension = os.path.splitext(src)[1]
+
+                # duplicate check
+                while True:
+                    filename = "".join(random.choices(string.ascii_letters + string.digits, k=30)) + extension
+                    if filename not in os.listdir(dst):
+                        break
+
+                os.rename(src, os.path.join(dst, filename))
+
+    return Fore.GREEN + "\nDone!"
+
+
+
+init(autoreset=True)
+fire.Fire(Stirge)
